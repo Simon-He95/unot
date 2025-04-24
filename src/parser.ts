d74c4b49ede9d9b17c1b3787bd0132a50ea114f6
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-24 09:06:14 +0800

    refactor: bundle dependencies

--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,7 @@
 import type { SFCTemplateBlock } from '@vue/compiler-sfc'
 import { parse as tsParser } from '@typescript-eslint/typescript-estree'
 import { getCurrentFileUrl, getOffsetFromPosition } from '@vscode-use/utils'
-import { parse } from '@vue/compiler-sfc/dist/compiler-sfc.esm-browser.js'
+import { parse } from '@vue/compiler-sfc'
 
 import * as vscode from 'vscode'
 
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,10 +3,21 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   target: 'node14',
   format: ['cjs'],
-  external: [
-    'vscode',
-  ],
+  external: ['vscode'],
   // minify: true,
   clean: true,
   platform: 'node', // 明确指定为 Node.js 平台
+  plugins: [
+    {
+      name: 'external-vue-sfc-compiler',
+      async resolveId(id, importer, options) {
+        if (importer?.includes('@vue/compiler-sfc')) {
+          const result = await this.resolve(id, importer, options)
+          if (!result) {
+            return { id, external: true }
+          }
+        }
+      },
+    },
+  ],
 })
